b3dfa762e6476cb96206f9fa4cf228df9467bc31
Tayyab <mtayyabsohail8@gmail.com>
2026-05-14 12:44:22 +0500

    changes

--- constants/school-identity.ts
+++ constants/school-identity.ts
@@ -4,7 +4,7 @@
  */
 
 export const SCHOOL_NAME = "Al-Fazal Public High School";
-export const SCHOOL_SHORT_NAME = "Al-Fazal School";
+export const SCHOOL_SHORT_NAME = "Al-Fazal llllllllll";
 export const SCHOOL_ADDRESS = "219RB Arshad Town Faisalabad";
 export const SCHOOL_PHONE = "0301 7025773";
 export const SCHOOL_TAGLINE = "Institution of Academic Excellence";
